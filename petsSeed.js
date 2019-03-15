8e734e3098befe95d6058b1dab470d131a2bd571
Matthew Weaber <matthewweaber89@gmail.com>
2019-03-14 20:44:00 -0400

    Adding JS Seeds

---
+++ petsSeed.js
@@ -0,0 +1,130 @@
+var petsSeed = [{
+        name: "Scarlet",
+        photo: 1,
+        breed: "Shiba Inu",
+        age: 2,
+        size: "Medium",
+        gender: 1,
+        location: "Kissimmee",
+        adopted: 1
+    },
+    {
+        name: "Murphy",
+        photo: 1,
+        breed: "Cairn Terrier",
+        age: 6,
+        size: "Small",
+        gender: 0,
+        location: "Orlando",
+        adopted: 1
+    },
+    {
+        name: "Rupert",
+        photo: 1,
+        breed: "Chocolate Lab",
+        age: 7,
+        size: "Large",
+        gender: 0,
+        "Orlando",
+        adopted: 1
+    },
+    {
+        name: "Sophie",
+        photo: 1,
+        breed: "Chiahuahua",
+        age: 4,
+        size: "Small",
+        gender: 1,
+        location: "Davenport",
+        adopted: 1
+    },
+    {
+        name: "Tina",
+        photo: 1,
+        breed: "Chiahuahua",
+        age: 5,
+        size: "Small",
+        gender: 1,
+        location: "Davenport",
+        adopted: 1
+    },
+    {
+        name: "Duke",
+        photo: 1,
+        breed: "German Shepherd",
+        age: 5,
+        size: "Large",
+        gender: 0,
+        location: "Ocoee",
+        adopted: 1
+    },
+    {
+        name: "Amos",
+        photo: 1,
+        breed: "Weimaraner",
+        age: 6,
+        size: "Large",
+        gender: 0,
+        location: "Orlando",
+        adopted: 1
+    },
+    {
+        name: "Annie",
+        photo: 1,
+        breed: "Golden Retriever",
+        age: 7,
+        size: "Large",
+        gender: 1,
+        location: "Kissimmee",
+        adopted: 1
+    },
+    {
+        name: "Diesel",
+        photo: 1,
+        breed: "Pitbull",
+        age: 7,
+        size: "Large",
+        gender: 0,
+        location: "Orlando",
+        adopted: 1
+    },
+    {
+        name: "Doodle",
+        photo: 1,
+        breed: "Chocolate Lab",
+        age: 2,
+        size: "Large",
+        gender: 0,
+        location: "Ocoee",
+        adopted: 1
+    },
+    {
+        name: "Walter",
+        photo: 1,
+        breed: "Pug",
+        age: 1,
+        size: "Small",
+        gender: 0,
+        location: "Davenport",
+        adopted: 1
+    },
+    {
+        name: "Poppy",
+        photo: 1,
+        breed: "Pug",
+        age: 7,
+        size: "Small",
+        gender: 0,
+        location: "Orlando",
+        adopted: 1
+    },
+    {
+        name: "Dozer",
+        photo: 1,
+        breed: "Bulldog",
+        age: 5,
+        size: "Medium",
+        location: "Ocoee",
+        adopted: 1
+    }
+]
